e2905d5eb6be536d8a6fbd1ee44091b8271f4143
jrj315 <jrj315@lehigh.edu>
2015-09-02 00:40:25 +0000

    updated hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,40 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//Jacob Jurecki
+//hw01
+//Welcome Class Java Program
+//
+//
+//  first compile the program
+//      java WelcomClass.java
+//  run the program
+//      java WelcomeClass//
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+    public static void main(String[] args)  {
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("^  ^  ^  ^  ^  ^");
+        System.out.println("V  V  V  V  V  V");
+        // could not fit last "\" in as it changed coloring of text from yellow to white
+        // actually changed your design as "/" and "\" characters are "illegal"
+        // in the terminal apparently. 
+        //lol
+        System.out.println("<-J--R--J--3--1--5->");
+        System.out.println("^  ^  ^  ^  ^  ^");
+        System.out.println("v  v  v  v  v  v");
+        System.out.println("test");
+        //test/testtest
+    }
+}
+
+//system would not run with lab01 as I believe I deleted the [M]/README.m which
+//somehow has an effect on this lab. Please understand if I am showing this
+//to you on 9/2 the day after it is due. Will have pictures of my progress to
+// show in class (wednesday) tomorrow.
+//the "git add*" command aint workin or registering Prof. Chen :(
+
+
+        
